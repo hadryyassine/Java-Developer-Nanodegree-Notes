d16dfa99c81293ad378c0b52bdbdcc1425e397ef
hadryyassine <hadryyassine@gmail.com>
2023-01-19 01:21:14 +0100

    binarysearch

---
+++ Java & spring basics/Java basics/Binarysearch/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sb= new int[]{1, 5, 7, 8, 9, 11,13, 18, 19, 20, 25};
+        int target=8;
+        System.out.print(binarysearch.findTarget(sb,target));
+
+    }
+}
